31636c1c613f8d3e38331939847b27ab15d2bffc
Lionel Guichard <lguichard@business-first.io>
2026-06-17 11:11:46 +0200

    Update forel.rb

--- Casks/forel.rb
+++ Casks/forel.rb
@@ -5,7 +5,7 @@ cask "forel" do
   sha256 arm:   "00d2f0dd9145d00aa20ec72ffde99af3a8bd518f5fdb86e87d4d8ee735c8a8c5",
          intel: "ac0848c9cab9ede609df8f65e7829570a656959fbba3e81520318ae3d69e851e"
 
-  url "https://github.com/forel-app/forel/releases/download/v#{version}/Forel-v#{version}-darwin-#{arch}.dmg"
+  url "https://github.com/forel-app/forel/releases/download/v0.1.0-alpha.8/Forel-v0.1.0-alpha.8-darwin-#{arch}.dmg"
   name "Forel"
   desc "macOS file-automation app that watches folders and runs rules"
   homepage "https://github.com/forel-app/forel"
